0eae78fbe9ca0677586a9916177405169670e553
Sarah Lawrence <s.j.lawrence8@gmail.com>
2018-06-30 00:23:59 +1000

    update site title to WIP name

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Default Starter',
+    title: 'Discover My Weekend',
   },
   plugins: ['gatsby-plugin-react-helmet', 'gatsby-plugin-react-next'],
 };
