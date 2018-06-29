65135cd6970019bb76a4ff8052ba715aee1dfc9d
yundifu <mymedicine77@gmail.com>
2018-06-29 21:53:19 +1000

    added button group

--- src/pages/landing-page.js
+++ src/pages/landing-page.js
@@ -2,7 +2,28 @@ import React from 'react';
 import Carousel from 'nuka-carousel';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
+import ButtonGroup from '../components/group';
 import './landing-page.css'
+
+
+const group = [
+  {
+    url: 'https://odysseyonline-img.rbl.ms/simage/http%3A%2F%2Fcdn1.theodysseyonline.com%2Ffiles%2F2015%2F11%2F09%2F635826395277009417-64976811_Single-Because.jpg/2000%2C2000/GVqnI8WV6hR3lYmF/img.jpg',
+    title: 'Single',
+    width: '40%',
+  },
+  {
+    url: 'http://www.roseparkpsychology.com.au/wp-content/uploads/2014/08/Family-Therapy.jpg',
+    title: 'Family',
+    width: '30%',
+  },
+  {
+    url: 'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/01-prince-harry-meghan-markle-1517172379.jpg?crop=1.00xw:0.377xh;0,0.0457xh&resize=768:*',
+    title: 'Couple',
+    width: '30%',
+  },
+];
+
 
 export default class extends React.Component {
     state = {
@@ -29,15 +50,7 @@ export default class extends React.Component {
         />
         </div>
         <div className='slide group'> 
-            <Button variant="contained" >
-            Single
-            </Button>
-            <Button variant="contained" color="primary" >
-                Family
-            </Button>
-            <Button variant="contained" color="secondary">
-                Couple
-            </Button>
+            <ButtonGroup data={group}/>
         
         </div>
         <div className='slide style'>  
