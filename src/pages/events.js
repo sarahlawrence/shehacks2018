9ab1af7ecbd64b18ee4ada05cfc6f67a041bf6d9
Sarah Lawrence <s.j.lawrence8@gmail.com>
2018-06-30 09:20:13 +1000

    sass style

--- src/pages/events.js
+++ src/pages/events.js
@@ -5,7 +5,7 @@ import * as RAW from '../../data.json';
 class EventsList extends React.Component {
   constructor(props) {
     super(props);
-    const selectedIds = [1,2,3,4,5,6,7,8]; // REPLACE
+    const selectedIds = [1,2,5,6]; // REPLACE
     const data = RAW.data.filter(x => selectedIds.includes(x.id));
     this.state = {
       data,
