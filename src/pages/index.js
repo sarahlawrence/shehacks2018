707ad192271f5bd40ad06c73fa3fdd602718dd3e
Sarah Lawrence <s.j.lawrence8@gmail.com>
2018-06-29 21:18:44 +1000

    init events page

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,6 +7,7 @@ const IndexPage = () => (
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
     <Link to="/page-2/">Go to page 2</Link>
+    <Link to="/events/">Events</Link>
   </div>
 )
 
