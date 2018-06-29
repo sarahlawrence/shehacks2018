8a396362f429dd5f008cb6331074704f3aa04d1c
Sarah Lawrence <s.j.lawrence8@gmail.com>
2018-06-30 00:51:12 +1000

    flip events and daylist to use real data

--- src/components/dayList.js
+++ src/components/dayList.js
@@ -2,36 +2,32 @@ import React from 'react'
 import CardBlock from './cardBlock'
 import Grid from '@material-ui/core/Grid';
 
-const eventA = {
-      "id": 2,
-      "name": "Freshie Community Garden",
-      "location": "Crown Reserve, Queenscliff",
-      "postcode": 2095,
-      "day": "Sunday",
-      "time": "Day",
-      "description": "Freshie Community Garden seeks to create an attractive, welcome, shared and safe place to grow fruit trees, vegetables and flowers using sustainable practices, and to promote social harmony and inclusion through building community capacity.",
-      "image": "https://files.northernbeaches.nsw.gov.au/sites/default/files/styles/gi--main-thumbnail/public/images/recreational-areas/2018-06-19-10-00//freshiecomgard.jpg?itok=3bH4eyss"
-};
+class DayList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
 
-const DayList = ({ day }) => (
-  <div>
-    <h1>{ day }</h1>
+  buildList() {
+    return this.props.data.map((e) => {
+      return (
+        <Grid item key={e.name}>
+          <CardBlock data={e} />
+        </Grid>
+      )
+    });
+  }
 
-    <Grid container spacing={8} justify='center'>
-      <Grid item>
-        <CardBlock data={eventA} />
-      </Grid>
-      <Grid item>
-        <CardBlock data={eventA} />
-      </Grid>
-      <Grid item>
-        <CardBlock data={eventA} />
-      </Grid>
-      <Grid item>
-        <CardBlock data={eventA} />
-      </Grid>
-    </Grid>
-  </div>
-)
+  render() {
+    return (
+      <div>
+        <h1>{ this.props.day }</h1>
+        <Grid container spacing={8} justify='center'>
+          {this.buildList()}
+        </Grid>
+      </div>
+    );
+  }
+}
 
-export default DayList
+export default DayList;
--- src/pages/events.js
+++ src/pages/events.js
@@ -1,11 +1,27 @@
-import React from 'react'
+import React from 'react';
 import DayList from '../components/dayList'
+import * as RAW from '../../data.json';
 
-const EventsList = () => (
-  <div>
-    <DayList day='saturday' />
-    <DayList day='sunday' />
-  </div>
-)
+class EventsList extends React.Component {
+  constructor(props) {
+    super(props);
+    const selectedIds = [1,2,3,4,5,6,7,8]; // REPLACE
+    const data = RAW.data.filter(x => selectedIds.includes(x.id));
+    this.state = {
+      data,
+    };
+  }
 
-export default EventsList
+  render() {
+    const saturdayData = this.state.data.filter(x => x.day === 'Saturday');
+    const sundayData = this.state.data.filter(x => x.day === 'Sunday');
+    return (
+      <div>
+        <DayList day='Saturday' data={saturdayData}/>
+        <DayList day='Sunday' data={sundayData}/>
+      </div>
+    );
+  }
+}
+
+export default EventsList;
