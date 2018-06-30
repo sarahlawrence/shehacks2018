2459b0ed02a66acec3479da7e67a31952f769fc6
Sarah Lawrence <s.j.lawrence8@gmail.com>
2018-06-30 10:14:29 +1000

    styling and color updates

--- src/components/imIn.js
+++ src/components/imIn.js
@@ -2,14 +2,30 @@ import React from 'react'
 import Typography from '@material-ui/core/Typography';
 import Checkbox from '@material-ui/core/Checkbox';
 
-const buttonText = "I'm in!";
-const ImIn = () => (
-  <div className='im-in'>
-    <Checkbox className='checkbox' style={{'color': 'white'}}/>
-    <p className='im-in-text'>
-      {buttonText}
-    </p>
-  </div>
-)
+class ImIn extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      checked: false,
+    }
+  }
+
+  handleClick() {
+    const oldState = this.state.checked;
+    this.setState({checked: !oldState});
+  }
+
+  render() {
+    const buttonText = "I'm in!";
+    return (
+      <div className='im-in' className={this.state.checked ? 'im-in checked': 'im-in'} style={{'color': 'white'}}>
+        <Checkbox className='checkbox' onClick={(e) => this.handleClick(e)} style={{'color': 'white'}}/>
+        <p className='im-in-text'>
+          {buttonText}
+        </p>
+      </div>
+    )
+  }
+}
 
 export default ImIn
