d8ed0369d8542bf83d86407c087c89e48a59a0c1
yundifu <mymedicine77@gmail.com>
2018-06-30 11:39:10 +1000

    Revert "added header banner"
    This reverts commit ccdefab193a5bdf5ac6478e0fad6b1e7213b5439.

--- src/components/header.js
+++ src/components/header.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import Link from 'gatsby-link'
-import Button from '@material-ui/core/Button';
 
 const Header = ({ siteTitle }) => (
   <header className='header'>
@@ -10,14 +9,6 @@ const Header = ({ siteTitle }) => (
           {siteTitle}
         </Link>
       </h1>
-
-      <div className='banner'>
-
-            <Button > about us </Button> 
-            <Button >why to join</Button>
-            <Button >contact us</Button>
-
-      </div>
     </div>
   </header>
 )
