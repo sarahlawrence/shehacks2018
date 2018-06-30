0a03ee4de6066480895fb93e25bfe6fc909fb100
Sarah Lawrence <s.j.lawrence8@gmail.com>
2018-06-30 12:45:41 +1000

    fixing white text and postcode

--- src/components/postcode.js
+++ src/components/postcode.js
@@ -26,13 +26,8 @@ class Postcode extends React.Component {
         <img src={PostcodeImg} className='full-img'/>
       </div>
       <div className='wrap-content'>
-<<<<<<< HEAD
         <h1 className='wrap-text'>Where would you like to spend your weekend?</h1>
         <InputLabel htmlFor="postcode">Postcode</InputLabel>
-=======
-        <h1 className="white">Where would you like to spend your weekend?</h1>
-        <InputLabel htmlFor="postcode" className="white">Postcode</InputLabel>
->>>>>>> 610041911f45899ae3a98255a56b8dfaf4c1221e
           <Input id="postcode" onChange={(e) => this.handleChange(e)}
             startAdornment={<InputAdornment position="start"></InputAdornment>}
           />
