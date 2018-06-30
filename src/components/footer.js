535b97960f6d6cd012230d1cf8250870e716d7ad
Janette Chiu <janettechiu@gmail.com>
2018-06-30 10:24:31 +1000

    footer

---
+++ src/components/footer.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const Footer = () => (
+  <div className="footer"
+  >
+    <div
+      style={{
+        margin: '0 auto',
+        maxWidth: 960,
+        padding: '1.45rem 1.0875rem',
+      }}
+    >
+      <h3 style={{ margin: 0 }}>
+          Thanks for discovering your weekend with us
+      </h3>
+      <p>© Copyright 2018</p>
+    </div>
+  </div>
+)
+
+export default Footer
